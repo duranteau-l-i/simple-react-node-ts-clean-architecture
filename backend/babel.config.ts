bbdb2a9b27a089ba8d1984e259416c615e34b3f8
ludovic duranteau <ludovic.duranteau@signaturelnd.com>
2020-12-26 13:52:35 +0000

    backend - add test framework jest

---
+++ backend/babel.config.ts
@@ -0,0 +1,5 @@
+/* eslint-disable prettier/prettier */
+
+module.exports = {
+  presets: ["@babel/preset-typescript"]
+};
---
+++ backend/src/test/unit/postsLoader.test.ts
@@ -0,0 +1,5 @@
+describe("Get post", () => {
+  it("1 + 1 = 2", () => {
+    expect(1 + 1).toEqual(2);
+  });
+});
